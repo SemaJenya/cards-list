7111c7b451cafd044b71a29c948d331b6147ba51
Evgeniia Semushina <semajenya96@gmail.com>
2024-12-01 22:39:19 +0200

    changed homepage and added hashRouter

--- src/index.js
+++ src/index.js
@@ -3,37 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from '../src/components/app/App.js';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import {
-  createHashRouter,
-  RouterProvider
-} from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  }
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <HashRouter >
+    <App />
+  </HashRouter>
+  // </React.StrictMode>
 );
-
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   // <React.StrictMode>
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>
-//   // </React.StrictMode>
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
