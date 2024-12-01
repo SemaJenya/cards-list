a7e2295e83d5e9de0a9145fff644b09761a595ba
Evgeniia Semushina <semajenya96@gmail.com>
2024-12-01 23:19:18 +0200

    fixed json homepage

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from '../src/components/app/App.js';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
